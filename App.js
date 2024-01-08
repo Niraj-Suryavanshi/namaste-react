022d71be5161779078e0c539dc6ab693a293b363
Sunil Suryavanshi <new@gmail.com>
2024-01-08 22:23:38 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-/**
- * 
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h1 tag</h2>
- * </div>
- * <div id="child2">
- *      <div id="child">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- * </div>
- * 
- * 
- * ReactElement(Object)=>HTML(Browser Understands)
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div",
@@ -22,7 +7,7 @@ const parent = React.createElement(
     [React.createElement(
         "div",
         { id: "child1" },
-        [React.createElement("h1", {}, "I a'm an h1 tag1"),
+        [React.createElement("h1", {}, "Namaste Javascript 🚀"),
         React.createElement("h2", {}, "I a'm an h2 tag2")]
     ), React.createElement(
         "div",
