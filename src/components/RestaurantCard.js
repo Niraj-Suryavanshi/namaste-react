10af1b131b218319b3bfb3a8e5ca5bb1330ab5bb
Sunil Suryavanshi <52320329+Niraj-Suryavanshi@users.noreply.github.com>
2024-01-13 22:40:37 +0530

    Added standard industry structure to the code

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,29 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const { cloudinaryImageId, name, cuisines, avgRating, sla, costForTwo } =
+    resData?.info;
+
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <div className="res-img">
+        <img
+          className="res-logo"
+          src={CDN_URL + cloudinaryImageId}
+          alt="res-logo"
+        />
+      </div>
+      <div className="res-desc">
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{sla.deliveryTime} minutes</h4>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCard;
