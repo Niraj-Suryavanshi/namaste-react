88a4f7dc71220c0e1530cb8ef90fd2a1b00a018a
Sunil Suryavanshi <52320329+Niraj-Suryavanshi@users.noreply.github.com>
2024-01-14 20:35:22 +0530

    added React hooks (useState()) functionality for finding top rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  //local state variables
+
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterdList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(filterdList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-main">
         <div className="res-container">
-          {resList.map((Restaurant) => (
+          {listOfRestaurants.map((Restaurant) => (
             <RestaurantCard key={Restaurant.info.id} resData={Restaurant} />
           ))}
         </div>
