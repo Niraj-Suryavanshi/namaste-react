d9cfe396641fb795ae4f10b2a989856f81c69468
Sunil Suryavanshi <52320329+Niraj-Suryavanshi@users.noreply.github.com>
2024-03-28 00:33:51 +0530

    Swiggy API testing success

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -16,26 +16,27 @@ const RestaurantMenu = () => {
     return <Shimmer />;
   }
 
-  const info = resInfo?.cards?.[0]?.card?.card?.info;
+  const info = resInfo?.cards?.[2]?.card?.card?.info;
+  console.log("The resinfo is :", info);
 
-  const itemCards1 =
-    resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card
-      .itemCards;
+  // const itemCards1 =
+  //   resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card
+  //     .itemCards;
 
-  const itemCards2 =
-    resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card
-      .itemCards;
+  // const itemCards2 =
+  //   resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card
+  //     .itemCards;
 
-  const finalItemCard = itemCards1 == null ? itemCards2 : itemCards1;
+  // const finalItemCard = itemCards1 == null ? itemCards2 : itemCards1;
 
   const categories =
-    resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards.filter(
+    resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards.filter(
       (c) =>
         c.card.card["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
 
-  console.log("categories is :", categories);
+  // console.log("categories is :", categories);
 
   const { name, cuisines, costForTwoMessage, avgRating } = info;
 
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,10 +1,16 @@
+import React from "react";
+import "./../../Shimmer.css";
+
 const Shimmer = () => {
   return (
-    <div>
-      <div className="shimmer-container">
+    <div className="shimmer-container">
+      <div className="shimmer-wrapper ">
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+      </div>
+      <div className="shimmer-wrapper">
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
